250a656be362795d2b4cfcec9e9120b2b602033f
Samuel Lange <samuelmanuel.lange@gmail.com>
2018-12-06 16:04:29 +0100

    I had a really hard time completing this task so i had to take many looks at the example

--- app.ts
+++ app.ts
@@ -1,12 +1,9 @@
-import {CREATED, BAD_REQUEST, UNAUTHORIZED} from 'http-status-codes';
-import * as loki from 'lokijs';
 import * as express from 'express';
-import * as basic from 'express-basic-auth';
+import * as loki from 'lokijs';
+import {CREATED, BAD_REQUEST, UNAUTHORIZED} from 'http-status-codes';
 
 var app = express();
 app.use(express.json());
-
-const adminFilter = basic({ users: { admin: 'P@ssw0rd!' }});
 
 const db = new loki(__dirname + '/db.dat', {autosave: true, autoload: true});
 let guests = db.getCollection('guests');
@@ -14,30 +11,29 @@ if (!guests) {
   guests = db.addCollection('guests');
 }
 
-app.get('/guests', adminFilter, (req, res) => {
-  res.send(guests.find());
+app.get('/guests', (req, res) => {
+    res.send(guests.find());
 });
 
 app.get('/party', (req, res, next) => {
-  res.send({
-    title: 'Happy new year!',
-    location: 'At my home',
-    date: new Date(2017, 0, 1)
-  });
-});
+    res.send({
+        title: 'Silvester Party!!!',
+        location: 'At my home',
+        date: new Date(12,31,2018)
+    })
+})
 
 app.post('/register', (req, res, next) => {
-  if (!req.body.firstName || !req.body.lastName) {
-    res.status(BAD_REQUEST).send('Missing mandatory member(s)');
-  } else {
-    const count = guests.count();
-    if (count < 10) {
-      const newDoc = guests.insert({firstName: req.body.firstName, lastName: req.body.lastName});
-      res.status(CREATED).send(newDoc);
-    } else {
-      res.status(UNAUTHORIZED).send('Sorry, max. number of guests already reached');
+    if(req.body.firstname === null || req.body.lastname === null){
+        res.status(BAD_REQUEST).send('Missing Data');
+    }else{
+        if(guests.count() < 10){
+            let newGuest = guests.insert({firstName: req.body.firstName, lastName: req.body.lastName});
+            res.status(CREATED).send(newGuest);
+        }else{
+            res.status(UNAUTHORIZED).send('Max number reached');
+        }
     }
-  }
-});
+})
 
 app.listen(8080, () => console.log('API is listening'));
